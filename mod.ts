b0f960795302a9b7f50c88b9771f434f899c25a6
Gabriel Cozma <me@gabs.eu.org>
2024-10-28 11:49:29 +0200

    feat: add new commands
    Precentage
    Remaining
    Decimal
    Remaining/Days

--- mod.ts
+++ mod.ts
@@ -6,17 +6,58 @@ const dayOfYear = Math.floor(diff / oneDay);
 
 export const handler = (req: Request): Response => {
   const url = new URL(req.url);
-  if (url.pathname === "/year-progress") {
+  if (url.pathname === "/") {
     const progress = (dayOfYear / 365) * 100;
     const body = JSON.stringify({
       progress: progress.toFixed(2) + "%",
       days: dayOfYear,
       remaining: (100 - progress).toFixed(2) + "%",
     });
-    return new Response(body, { status: 200, headers: { "Content-Type": "application/json" } });
-  } else if (url.pathname === "/day-of-year") {
+    return new Response(body, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } else if (url.pathname === "/days") {
     const body = JSON.stringify({ dayOfYear });
-    return new Response(body, { status: 200, headers: { "Content-Type": "application/json" } });
+    return new Response(body, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } else if (url.pathname === "/precentage") {
+    const progress = (dayOfYear / 365) * 100;
+    const body = JSON.stringify({
+      procentage: progress.toFixed(2) + "%",
+    });
+    return new Response(body, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } else if (url.pathname === "/remaining") {
+    const progress = (dayOfYear / 365) * 100;
+    const body = JSON.stringify({
+      remaining: (100 - progress).toFixed(2) + "%",
+    });
+    return new Response(body, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } else if (url.pathname === "/decimal") {
+    const progress = dayOfYear / 365;
+    const body = JSON.stringify({
+      decimal: progress.toFixed(2),
+    });
+    return new Response(body, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } else if (url.pathname === "/remaining/days") {
+    const body = JSON.stringify({
+      remaining: 365 - dayOfYear,
+    });
+    return new Response(body, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } else {
     return new Response("Not Found", { status: 404 });
   }
